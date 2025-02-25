8cb6006638c93289586118b24630ff36958d8fda
Bhaskar Reddy <164631537+Bhas90@users.noreply.github.com>
2025-02-25 17:37:01 +0530

    Update Navbar.jsx

--- frontend/src/Navbar.jsx
+++ frontend/src/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
       </div>
       
       {/* Phone number with mobile icon */}
-      <a href="tel:+01902921475" className="text-lg flex items-center hover:text-gray-200">
+      <a href="tel:01902921475" className="text-lg flex items-center hover:text-gray-200">
         <FaPhoneAlt className="mr-2" /> +01902921475
       </a>
     </header>
