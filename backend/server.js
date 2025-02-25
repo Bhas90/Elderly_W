8c60adf3a133a6fa6ed3b039e806f8da3ae06f86
Bhaskar Reddy <164631537+Bhas90@users.noreply.github.com>
2025-02-25 13:10:26 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const app = express();
 const port = 5000;
 
 app.use(cors({
-  origin: ["https://wolverhampton.britishelderlycare.com"], // Add frontend domain
+  origin: "*", // Add frontend domain
   methods: ["POST", "GET"],
   allowedHeaders: ["Content-Type"]
 }));
